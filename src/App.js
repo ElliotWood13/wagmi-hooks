bafda546793bd8f9c25bc8cdbad5a1cc74d4e433
Elliot Wood <43952853+ElliotWood13@users.noreply.github.com>
2023-04-02 19:10:16 +0100

    look into other connectors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import "./App.css";
 import { Profile } from "./Profile";
-import { WagmiConfig, createClient } from "wagmi";
-import { getDefaultProvider } from "ethers";
+import { WagmiConfig, createClient, configureChains, mainnet } from "wagmi";
 
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { Buffer } from "buffer";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet],
+  [alchemyProvider({ apiKey: "yourAlchemyApiKey" }), publicProvider()]
+);
+
+// Set up client
 const client = createClient({
   autoConnect: true,
-  provider: getDefaultProvider(),
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "wagmi",
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
 });
 
 function App() {
--- src/Profile.js
+++ src/Profile.js
@@ -1,19 +1,47 @@
-import { useAccount, useConnect, useDisconnect } from "wagmi";
-import { InjectedConnector } from "wagmi/connectors/injected";
+import {
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useEnsAvatar,
+  useEnsName,
+} from "wagmi";
 
 export function Profile() {
-  const { address, isConnected } = useAccount();
-  const { connect } = useConnect({
-    connector: new InjectedConnector(),
-  });
+  const { address, connector, isConnected } = useAccount();
+  const { data: ensAvatar } = useEnsAvatar({ address });
+  const { data: ensName } = useEnsName({ address });
+  const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect();
   const { disconnect } = useDisconnect();
 
-  if (isConnected)
+  if (isConnected) {
     return (
       <div>
-        Connected to {address}
-        <button onClick={() => disconnect()}>Disconnect</button>
+        <img src={ensAvatar} alt="ENS Avatar" />
+        <div>{ensName ? `${ensName} (${address})` : address}</div>
+        <div>Connected to {connector.name}</div>
+        <button onClick={disconnect}>Disconnect</button>
       </div>
     );
-  return <button onClick={() => connect()}>Connect Wallet</button>;
+  }
+
+  return (
+    <div>
+      {connectors.map((connector) => (
+        <button
+          disabled={!connector.ready}
+          key={connector.id}
+          onClick={() => connect({ connector })}
+        >
+          {connector.name}
+          {!connector.ready && " (unsupported)"}
+          {isLoading &&
+            connector.id === pendingConnector?.id &&
+            " (connecting)"}
+        </button>
+      ))}
+
+      {error && <div>{error.message}</div>}
+    </div>
+  );
 }
